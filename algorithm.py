1197c84ba853790f73797e10bc09d775b455ff1a
shinde_aditya <shindeaditya835@gmail.com>
2025-04-01 23:52:38 +0530

    algorithm

---
+++ algorithm.py
@@ -0,0 +1,151 @@
+# #Swapping of two no. with third variable
+# a=int(input("Num1:"))
+# b=int(input("Num2:"))
+
+# temp=a
+# a=b
+# b=temp
+
+# print(a)
+# print(b)
+
+
+# #Swapping of two no.without third variable
+# a=int(input("Num1:"))
+# b=int(input("Num2:"))
+
+# a=a+b #(100+200=300)
+# b=a-b  #(300-200=100)
+# a=a-b  #(300-100=200)
+
+# print(a)
+# print(b)
+
+
+# #WAP to calculate three paper marks
+# p1=int(input("paper1 marks:"))
+# p2=int(input("paper2 marks:"))
+# p3=int(input("paper3 marks:"))
+
+# total=p1+p2+p3;
+# average=float(total/3.0)
+
+# print(total)
+# print(average)
+
+
+# #Simple interest
+# pa=int(input("Principle amount:"))
+# tm =int(input("Time:"))
+# r=int(input("Rate of interest:"))
+
+# simpleInterest=float((pa*tm*r)/100)
+# print(simpleInterest)
+
+
+# #Area of circle
+# pi=3.14
+# r=int(input("Enter the radius:"))
+# area=pi*r*r
+# print("Area of circle is:-",area)
+
+
+
+# h=int(input("Enter height in feet:"))
+# inch=h*12
+# cm=inch*2.54
+
+# print("inch:-",inch)
+# print("cm:-",cm)
+
+
+
+# centi=int(input("Enter the temparature in centigrade: "))
+
+# f=1.8*centi+240
+
+# print("Farhrenheit:",f)
+
+
+# #
+# p1=int(input("Enter paper-1 marks:"))
+# p2=int(input("Enter paper-2 marks:"))
+# p3=int(input("Enter paper-3 marks:"))
+# p4=int(input("Enter paper-4 marks:"))
+# p5=int(input("Enter paper-5 marks:"))
+
+# if p1>=40 and p2>=40 and p3>=40 and p4>=40 and p5>=40:
+#   print("Pass")
+# else:
+#   print("Fail")
+
+# total=p1+p2+p3+p4+p5
+# percentage=total/5.0
+
+# # gender=input("Enter your gender (Male/Female):")
+# # if percentage>=60 and gender=="Male":
+# #   print("You are eligible for placement")
+# # elif percentage>=60 and gender=="Female":
+# #   print("You are eligible")
+# # else:
+# #   print("You are nort elegible")
+
+# print("Total:",total)
+# print("Percentage:",percentage)
+
+# if(percentage>80 and percentage<=100):
+#   print("Grade A")
+# elif percentage>60 and percentage<=80:
+#   print("Grade B")
+# elif percentage>40 and percentage<=60:
+#   print("C")
+# else:
+#   print("Fail")
+
+
+#   #Nested if else
+# a=int(input("Enter the value of A:"))
+# b=int(input("Enter the value of B:"))
+# c=int(input("Enter the value of C:"))
+
+# if a>b:
+#   if a>c:
+#     print(a," is greater")
+#   else:
+#     print(c," is greater")
+
+# else:
+#   if b>c:
+#     print(b," is greater")
+#   else:
+#     print(c," is greater")
+
+
+
+    
+# #Leap year
+# year=int(input("Enter the year:"))
+# if year%4==0 and year%100==0 and year%400==0:
+#   print(year," is the leap year")
+# else:
+#   print(year," is not leap year")
+
+
+#   #Check enter character is
+# ch=ord(input("Enter any character:"))
+
+# if ch>=65 and ch<=90:
+#   print("Uppercase")
+# elif ch>=97 and ch<=122:
+#   print("Lowercase")
+# else:
+#   print("Special character")
+
+# # if c.islower():
+# #   print("Lowercase")
+# # elif c.isupper():
+# #   print("Uppercase")
+# # elif c.isdigit():
+# #   print("Digit")
+# # else:
+# #   print("Special Character")
