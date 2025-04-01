7d9b14f9a993c215d7c396a3087ead65fad73f7c
shinde_aditya <shindeaditya835@gmail.com>
2025-04-01 23:57:02 +0530

    ds

---
+++ data_structure.py
@@ -0,0 +1,22 @@
+# #Reverse the string
+# s=input("Enter the string:")
+
+
+# print(s[::-1])
+
+# #palindrome
+# s=input("Enter the string:")
+
+# if s == s[::-1]:
+#   print("Palindromic string")
+# else:
+#   print('Not palindromic string')
+
+
+# #
+# name='Programming'
+# emp =''
+# for i in name:
+#   if i not in emp:
+#     emp+=i
+# print(emp)
