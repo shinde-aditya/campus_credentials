83fb42e80b45dcee0da743fe220648c5812c867c
shinde_aditya <shindeaditya835@gmail.com>
2025-04-01 23:55:36 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,80 @@
+# #for(initialization;condition;inc/dec)
+
+# for i in range(1,10,2):#i=0
+#   print(i)
+
+
+
+# for i in range(5,0,-1):#i=0
+#   print(i)
+
+
+#   for i in range(1,11):#i=0
+#   print(i*2 ," ",i*3," ",i*4," ",i*5," ",i*6," ",i*7," ",i*8," ",i*9," ",i*10)
+
+# print("---------------------------------------------------------------------")
+# for i in range(1,11):#i=0
+#   print(i*11," ",i*12 ," ",i*13," ",i*14," ",i*15," ",i*16," ",i*17," ",i*18," ",i*19," ",i*20)
+
+
+# #
+# count=0
+# for i in range(9):
+#   if i%2==0:
+#     print(i)
+#   else:
+#     print(i)
+#     count+=1;
+
+# print("Count:",count)
+
+
+# #find()
+# #return the first index of given string
+# s="help4code is a best platform for practicing programming"
+# print(s.find("help4code"))
+# print(s.find("python"))
+# print(s.find("programming"))
+
+
+# # join()
+# s="Aditya","Soham","Akshay"
+# m='|'.join(s)
+# print(m)
+
+
+# s="Python is a high level Language"
+# print(s.lower())
+# print(s.upper())
+# print(s.swapcase())
+# print(s.title())
+# print(s.capitalize())
+
+
+# phy=50
+# chem= 70
+# maths=80
+
+# print("Physics={} chemisrty={} Math={}".format(phy,chem,maths))
+# print("Physics={0} chemisrty={1} Math={2}".format(phy,chem,maths))
+# print("Physics={x} chemisrty={y} Math={z}".format(x=phy,y=chem,z=maths))
+
+# total=phy+chem+maths
+# print("Total marks ",f"(total)")
+# print("Roll no","7".zfill(4))
+
+
+# print("aditya123".isalnum())
+# print("aditya".isalpha())
+# print("123f".isdigit())
+# print("aditya123".isalnum())
+# print("aditya".islower())
+# print(" ".islower())
+
+# print("ADITYA".isupper())
+# print("My Name Is Aditya".istitle())
+# print(" ".istitle())
+# print(" ".isspace())
+# print("aditya123".isalnum())
+# print("Hello".startswith("He"))
+# print("Hello".endswith("lo"))
